5ba8d9c2bbda8f3d76e865c8f47950d590347a5b
SamuelOsp <avilaospina1@gmail.com>
2025-09-15 10:20:53 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyWally',
+  webDir: 'www'
+};
+
+export default config;
